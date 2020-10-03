00f16102c1d844443442016f1233235496ceddf3
uriel melendez <uriel.melendez@outlook.es>
2020-10-03 15:25:11 -0500

    Adding Endpoint configuration

---
+++ src/main/java/com/in28minutes/soap/CourseDetailsEndpoint.java
@@ -0,0 +1,29 @@
+package com.in28minutes.soap;
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.in28minutes.courses.GetCourseDetailsRequest;
+import com.in28minutes.courses.GetCourseDetailsResponse;
+
+@Endpoint
+public class CourseDetailsEndpoint {
+
+	//method
+	//input - GetCourseDetailsRequest
+	//output - GetCourseDetailsResponse
+	//name space http://in28minutes.com/courses
+	//name GetCourseDetailsRequest
+	@PayloadRoot(namespace = "http://in28minutes.com/courses",
+			localPart ="GetCourseDetailsRequest")
+	@ResponsePayload
+	public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) {
+		GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+		
+		
+		return response;
+	}
+	
+}

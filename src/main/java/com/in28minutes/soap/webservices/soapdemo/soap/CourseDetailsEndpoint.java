306d4e9073c88a120e69c339313a2185d8dd9636
uriel melendez <uriel.melendez@outlook.es>
2020-10-03 16:24:50 -0500

    Adding Spring web service configuration

--- src/main/java/com/in28minutes/soap/webservices/soapdemo/soap/CourseDetailsEndpoint.java
+++ src/main/java/com/in28minutes/soap/webservices/soapdemo/soap/CourseDetailsEndpoint.java
@@ -1,10 +1,11 @@
-package com.in28minutes.soap;
+package com.in28minutes.soap.webservices.soapdemo.soap;
 
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import com.in28minutes.courses.CourseDetails;
 import com.in28minutes.courses.GetCourseDetailsRequest;
 import com.in28minutes.courses.GetCourseDetailsResponse;
 
@@ -21,8 +22,14 @@ public class CourseDetailsEndpoint {
 	@ResponsePayload
 	public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) {
 		GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+	
+		CourseDetails courseDetails = new CourseDetails();
+		courseDetails.setId(request.getId());
+		courseDetails.setName("Java in 28 minutes");
+		courseDetails.setDescription("Learning java in 28 steps");
 		
 		
+		response.setCourseDetails(courseDetails);
 		return response;
 	}
 	
